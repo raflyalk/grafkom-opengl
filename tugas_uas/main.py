9fc856790017ba6a73b224dd8b010a5188501439
alghi <13516038@std.stei.itb.ac.id>
2019-05-01 16:09:54 +0700

    Init particles system

---
+++ tugas_uas/main.py
@@ -0,0 +1,139 @@
+import glfw
+from OpenGL.GL import *
+import ShaderLoader
+import numpy
+import pyrr
+from PIL import Image
+from ObjLoader import *
+
+def window_resize(window, width, height):
+    glViewport(0, 0, width, height)
+
+def main():
+
+    # initialize glfw
+    if not glfw.init():
+        return
+
+    w_width, w_height = 800, 600
+
+    #glfw.window_hint(glfw.RESIZABLE, GL_FALSE)
+
+    window = glfw.create_window(w_width, w_height, "My OpenGL window", None, None)
+
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    glfw.set_window_size_callback(window, window_resize)
+
+    obj = ObjLoader()
+    obj.load_model("res/car_small.obj")
+
+    texture_offset = len(obj.vertex_index)*12
+    normal_offset = (texture_offset + len(obj.texture_index)*8)
+
+    shader = ShaderLoader.compile_shader("shaders/vert_shader.vs", "shaders/frag_shader.fs")
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, obj.model.itemsize * len(obj.model), obj.model, GL_STATIC_DRAW)
+
+    #positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, obj.model.itemsize * 3, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(0)
+    #textures
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, obj.model.itemsize * 2, ctypes.c_void_p(texture_offset))
+    glEnableVertexAttribArray(1)
+    #normals
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, obj.model.itemsize * 3, ctypes.c_void_p(normal_offset))
+    glEnableVertexAttribArray(2)
+
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    # Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    # Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # load image
+    image = Image.open("res/metal.jpg")
+    flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = numpy.array(list(flipped_image.getdata()), numpy.uint8)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+    glEnable(GL_TEXTURE_2D)
+
+    glUseProgram(shader)
+
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+    glEnable(GL_DEPTH_TEST)
+    #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+    view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -3.0]))
+    projection = pyrr.matrix44.create_perspective_projection_matrix(65.0, w_width / w_height, 0.1, 100.0)
+    model = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, 0.0]))
+
+    view_loc = glGetUniformLocation(shader, "view")
+    proj_loc = glGetUniformLocation(shader, "projection")
+    model_loc = glGetUniformLocation(shader, "model")
+    transform_loc = glGetUniformLocation(shader, "transform")
+    light_loc = glGetUniformLocation(shader, "light")
+
+    glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+
+    # PARTICLES START HERE
+    shader_particles = ShaderLoader.compile_shader("shaders/vertex.vs", "shaders/fragment.fs")
+    
+    nr_particles = 500
+    particles = []
+    
+    for i in range(nr_particles):
+        particles.append(Particle())
+
+
+    counter_x = 0
+    counter_y = 0
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        rot_x = pyrr.Matrix44.from_x_rotation(0.9 * counter_x )
+        rot_y = pyrr.Matrix44.from_y_rotation(0.9 * counter_y )
+        if (glfw.get_key(window,glfw.KEY_UP) == glfw.PRESS):
+            counter_x-=0.01
+        if (glfw.get_key(window,glfw.KEY_DOWN) == glfw.PRESS):
+            counter_x+=0.01
+        if (glfw.get_key(window,glfw.KEY_RIGHT) == glfw.PRESS):
+            counter_y+=0.01
+        if (glfw.get_key(window,glfw.KEY_LEFT) == glfw.PRESS):
+            counter_y-=0.01
+        glUniformMatrix4fv(transform_loc, 1, GL_FALSE, rot_x * rot_y)
+        glUniformMatrix4fv(light_loc, 1, GL_FALSE, rot_x * rot_y)
+
+        glDrawArrays(GL_TRIANGLES, 0, len(obj.vertex_index))
+
+        nr_new_particles = 2
+        # Add new particles
+        for i in range(nr_new_particles):
+            unusedParticle = FirstUnusedParticle()
+            RespawnParticle(particles[unusedParticle], object, offset)
+        
+        # Update all particles
+        for i in range(nr_particles):
+            Particle &p = particles[i]
+            p.Life -= dt; # reduce life
+            if (p.Life > 0.0f):
+                # particle is alive, thus update
+                p.Position -= p.Velocity * dt
+                p.Color.a -= dt * 2.5
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
